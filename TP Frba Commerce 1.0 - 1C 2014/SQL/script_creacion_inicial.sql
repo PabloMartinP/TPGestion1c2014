225515409df21def796a429afac83e90946e82cc
Fernando Tadakuma <fernando.tadakuma@gmail.com>
2014-05-04 12:00:02 -0300

    +FUNCIONALIDAD
    Creacion de la tabla FUNCIONALIDAD
    Creacion de constraints de la tabla FUNCIONALIDAD
    Insercion de registros de la tabla FUNCIONALIDAD

--- TP Frba Commerce 1.0 - 1C 2014/SQL/script_creacion_inicial.sql
+++ TP Frba Commerce 1.0 - 1C 2014/SQL/script_creacion_inicial.sql
@@ -21,6 +21,12 @@ rol_nombre nvarchar(50) not null,
 rol_habilitado bit default 1
 );
 
+/****** Creacion de la tabla FUNCIONALIDAD ******/
+create table MAS_INSERTIVO.FUNCIONALIDAD
+(
+fun_id int identity(1,1), 
+fun_nombre nvarchar(50) not null
+);
 
 /*************************************/
 /****** CREACION DE CONSTRAINTS ******/
@@ -30,10 +36,16 @@ rol_habilitado bit default 1
 alter table MAS_INSERTIVO.ROL add constraint pk_rol primary key(rol_id);
 alter table MAS_INSERTIVO.ROL add constraint uc_rol_nombre unique(rol_nombre);
 
+/****** Creacion de constraints para la tabla FUNCIONALIDAD ******/
+alter table MAS_INSERTIVO.FUNCIONALIDAD add constraint pk_funcionalidad primary key(fun_id);
+alter table MAS_INSERTIVO.FUNCIONALIDAD add constraint uc_fun_nombre unique(fun_nombre);
 
 /**********************************/
 /****** CREACION DE TRIGGERS ******/
 /**********************************/
+
+/****** Creacion de triggers para la tabla ROL ******/
+-- TRIGGER inhabilitar rol
 
 
 /********************************/
@@ -44,7 +56,29 @@ alter table MAS_INSERTIVO.ROL add constraint uc_rol_nombre unique(rol_nombre);
 insert into MAS_INSERTIVO.ROL
 (rol_nombre)
 values
-('Empresa'),('Administrativo'),('Cliente');
+('Empresa'),
+('Administrativo'),
+('Cliente');
+
+/****** Insercion de datos en la tabla FUNCIONALIDAD ******/
+insert into MAS_INSERTIVO.FUNCIONALIDAD
+(fun_nombre)
+values
+--('Login y seguridad'),
+('ABM de Rol'),
+('Registro de Usuario'),
+('ABM de Cliente'),
+('ABM de Empresa'),
+('ABM de Rubro'),
+('ABM Visibilidad De Publicación'),
+('Generar Publicación'),
+('Editar Publicación'),
+('Gestión de Preguntas'),
+('Comprar/Ofertar'),
+('Historial del Cliente'),
+('Calificar al Vendedor'),
+('Facturar Publicaciones'),
+('Listado Estadístico');
 
 
 /*********************************/
