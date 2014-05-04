e86c1a9b2c516147397aee598f36fc843dfe49e1
Fernando Tadakuma <fernando.tadakuma@gmail.com>
2014-05-04 12:24:16 -0300

    +ROL_FUNCIONALIDAD
    Creacion de la tabla ROL_FUNCIONALIDAD.
    Creacion de constraints de la tabla ROL_FUNCIONALIDAD.

--- TP Frba Commerce 1.0 - 1C 2014/SQL/script_creacion_inicial.sql
+++ TP Frba Commerce 1.0 - 1C 2014/SQL/script_creacion_inicial.sql
@@ -18,14 +18,21 @@ create table MAS_INSERTIVO.ROL
 (
 rol_id int identity(1,1),
 rol_nombre nvarchar(50) not null,
-rol_habilitado bit default 1
+rol_habilitado bit default 1 not null
 );
 
 /****** Creacion de la tabla FUNCIONALIDAD ******/
 create table MAS_INSERTIVO.FUNCIONALIDAD
 (
-fun_id int identity(1,1), 
-fun_nombre nvarchar(50) not null
+func_id int identity(1,1), 
+func_nombre nvarchar(50) not null
+);
+
+/****** Creacion de la tabla ROL_FUNCIONALIDAD ******/
+create table MAS_INSERTIVO.ROL_FUNCIONALIDAD
+(
+rolfunc_rol_id int not null, 
+rolfunc_func_id int not null
 );
 
 /*************************************/
@@ -37,8 +44,13 @@ alter table MAS_INSERTIVO.ROL add constraint pk_rol primary key(rol_id);
 alter table MAS_INSERTIVO.ROL add constraint uc_rol_nombre unique(rol_nombre);
 
 /****** Creacion de constraints para la tabla FUNCIONALIDAD ******/
-alter table MAS_INSERTIVO.FUNCIONALIDAD add constraint pk_funcionalidad primary key(fun_id);
-alter table MAS_INSERTIVO.FUNCIONALIDAD add constraint uc_fun_nombre unique(fun_nombre);
+alter table MAS_INSERTIVO.FUNCIONALIDAD add constraint pk_funcionalidad primary key(func_id);
+alter table MAS_INSERTIVO.FUNCIONALIDAD add constraint uc_func_nombre unique(func_nombre);
+
+/****** Creacion de constraints para la tabla ROL_FUNCIONALIDAD ******/
+alter table MAS_INSERTIVO.ROL_FUNCIONALIDAD add constraint pk_rol_funcionalidad primary key(rolfunc_rol_id, rolfunc_func_id);
+alter table MAS_INSERTIVO.ROL_FUNCIONALIDAD add constraint fk_rolfunc_rol_id foreign key(rolfunc_rol_id) references MAS_INSERTIVO.ROL(rol_id);
+alter table MAS_INSERTIVO.ROL_FUNCIONALIDAD add constraint fk_rolfunc_func_id foreign key(rolfunc_func_id) references MAS_INSERTIVO.FUNCIONALIDAD(func_id);
 
 /**********************************/
 /****** CREACION DE TRIGGERS ******/
@@ -62,7 +74,7 @@ values
 
 /****** Insercion de datos en la tabla FUNCIONALIDAD ******/
 insert into MAS_INSERTIVO.FUNCIONALIDAD
-(fun_nombre)
+(func_nombre)
 values
 --('Login y seguridad'),
 ('ABM de Rol'),
@@ -79,6 +91,8 @@ values
 ('Calificar al Vendedor'),
 ('Facturar Publicaciones'),
 ('Listado Estadístico');
+
+/****** Insercion de datos en la tabla ROL_FUNCIONALIDAD ******/
 
 
 /*********************************/
