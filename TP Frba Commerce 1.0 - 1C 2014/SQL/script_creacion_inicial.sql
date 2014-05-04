d31118bc64676c149b57f65675285467de11c555
Fernando Tadakuma <fernando.tadakuma@gmail.com>
2014-05-04 11:29:11 -0300

    +ROL
    Creacion de la tabla ROL
    Creacion de las constraints de la tabla ROL
    Insercion de los roles 'Empresa', 'Administrativo', 'Cliente'

--- TP Frba Commerce 1.0 - 1C 2014/SQL/script_creacion_inicial.sql
+++ TP Frba Commerce 1.0 - 1C 2014/SQL/script_creacion_inicial.sql
@@ -1,5 +1,4 @@
 use [GD1C2014];
-go
 
 /****** Creacion del esquema MAS_INSERTIVO ******/
 if not exists (
@@ -9,5 +8,46 @@ if not exists (
 begin
 	exec ('create schema [MAS_INSERTIVO] authorization [gd]');
 end
-go
+
+/********************************/
+/****** CREACION DE TABLAS ******/
+/********************************/
+
+/****** Creacion de la tabla ROL ******/
+create table MAS_INSERTIVO.ROL
+(
+rol_id int identity(1,1),
+rol_nombre nvarchar(50) not null,
+rol_habilitado bit default 1
+);
+
+
+/*************************************/
+/****** CREACION DE CONSTRAINTS ******/
+/*************************************/
+
+/****** Creacion de constraints para la tabla ROL ******/
+alter table MAS_INSERTIVO.ROL add constraint pk_rol primary key(rol_id);
+alter table MAS_INSERTIVO.ROL add constraint uc_rol_nombre unique(rol_nombre);
+
+
+/**********************************/
+/****** CREACION DE TRIGGERS ******/
+/**********************************/
+
+
+/********************************/
+/****** MIGRACION DE DATOS ******/
+/********************************/
+
+/****** Insercion de datos en la tabla ROL ******/
+insert into MAS_INSERTIVO.ROL
+(rol_nombre)
+values
+('Empresa'),('Administrativo'),('Cliente');
+
+
+/*********************************/
+/****** CREACION DE INDICES ******/
+/*********************************/
 
