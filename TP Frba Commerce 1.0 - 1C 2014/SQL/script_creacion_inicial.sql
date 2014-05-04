6735678bb70a7b2e6bca72bf75c7d6a578a2e010
Fernando Tadakuma <fernando.tadakuma@gmail.com>
2014-05-04 10:01:28 -0300

    +Esquema MAS_INSERTIVO
    Creacion del esquema MAS_INSERTIVO

--- TP Frba Commerce 1.0 - 1C 2014/SQL/script_creacion_inicial.sql
+++ TP Frba Commerce 1.0 - 1C 2014/SQL/script_creacion_inicial.sql
@@ -1,3 +1,13 @@
-use GD1C2014
+use [GD1C2014];
+go
+
+/****** Creacion del esquema MAS_INSERTIVO ******/
+if not exists (
+	select	name
+	from	sys.schemas
+	where	name = 'MAS_INSERTIVO' )
+begin
+	exec ('create schema [MAS_INSERTIVO] authorization [gd]');
+end
 go
 
