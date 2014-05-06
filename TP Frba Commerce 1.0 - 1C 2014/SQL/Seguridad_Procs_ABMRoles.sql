e3ae9e9479c48412397101bc356e5fae4e50e946
PabloMartinP <pablo.penialoza@gmail.com>
2014-05-06 10:20:46 -0300

    update scripts ABMRol

--- TP Frba Commerce 1.0 - 1C 2014/SQL/Seguridad_Procs_ABMRoles.sql
+++ TP Frba Commerce 1.0 - 1C 2014/SQL/Seguridad_Procs_ABMRoles.sql
@@ -1,82 +1,80 @@
 --exec Funcionalidad_BuscarPorRol 1
-create proc Funcionalidad_BuscarPorRol
+create proc MAS_INSERTIVO.funcionalidad_BuscarPorRol
 @Rol_ID int
 as
 begin	
-	select f.fun_ID, 
-		f.fun_nombre, 
+	select f.func_ID, 
+		f.func_nombre, 
 		(CASe p.Rol_id when @Rol_ID then '1' else '0' end ) as Permitido 
-		  from funcionalidades f 
-	left join (select * from Roles_Funcionalidades where rol_id=@Rol_ID) p on f.fun_id=p.fun_id
+		  from MAS_INSERTIVO.Funcionalidad f 
+	left join (select * from MAS_INSERTIVO.rol_funcionalidad where rol_id=@Rol_ID) p on f.func_id=p.func_id
 	
 end
 go-------------------------------------
-create proc Rol_Agregar  
+create proc MAS_INSERTIVO.Rol_Agregar  
 @Nombre nvarchar(50), 
 @Habilitado bit
 as  
 begin  
---select * from Roles  
-  
- INSERT INTO Roles  
+ INSERT into MAS_INSERTIVO.rol  
  (Rol_Nombre, Rol_Habilitado)
  VALUES (@Nombre, @Habilitado)   
    
  select @@IDENTITY as 'ID'  
 end  
 go--------------------------
-create proc [Permiso_Agregar]
+create proc MAS_INSERTIVO.[Permiso_Agregar]
 @Rol_ID int, 
 @Fun_ID int
 as
 begin
-	insert into Roles_Funcionalidades
+	insert into MAS_INSERTIVO.rol_funcionalidad
 	values
 	(@Rol_ID, @Fun_ID)
 	
 end
 go------------------------------
-CREATE view [vw_roles]
+CREATE view MAS_INSERTIVO.vw_rol
 as
-select Rol_ID, Rol_Nombre, Rol_Habilitado from Roles 
-where Rol_Eliminado=0
+select Rol_ID, Rol_Nombre, Rol_Habilitado from MAS_INSERTIVO.rol 
+--where Rol_Eliminado=0
 go-------------------------------
-CREATE proc [Rol_Filtrar]  
-@ROl_Nombre nvarchar(50)  
+CREATE proc MAS_INSERTIVO.[Rol_Filtrar]  
+@ROl_Nombre nvarchar(50)
 as  
 begin  
- select * from vw_roles r   
+ select * from MAS_INSERTIVO.vw_rol r   
  where   
   r.Rol_Nombre like '%' +@ROl_Nombre + '%'  
 --  and p.Tipo_Nombre like ''%'' + @tipo_nombre + '%'   
 end  
 go-------------------------------
-create proc [Rol_Buscar]  
+create proc MAS_INSERTIVO.[Rol_Buscar]  
 @Rol_ID int  
 as  
 begin   
  select r.Rol_ID, r.Rol_Nombre, Rol_Habilitado
-  from roles r
- where r.Rol_ID=@Rol_ID and r.Rol_Eliminado=0
+  from MAS_INSERTIVO.rol r
+ where r.Rol_ID=@Rol_ID --and r.Rol_Eliminado=0
 end  
 go--------------------------
-create proc [Rol_Modificar]  
+create proc MAS_INSERTIVO.[Rol_Modificar]  
 @ID int,   
 @Nombre nvarchar(50), 
 @Habilitado bit
 as  
 begin  
-UPDATE Roles  
+update MAS_INSERTIVO.rol  
    SET Rol_Nombre = @Nombre, 
 	Rol_Habilitado = @Habilitado
  WHERE Rol_ID=@ID
  
 end
 go--------------------------------
-create proc [Permiso_EliminarPorRol]
+create proc MAS_INSERTIVO.[Permiso_EliminarPorRol]
 @Rol_ID int
 as
 begin
-	delete from Roles_Funcionalidades
+	delete from MAS_INSERTIVO.rol_funcionalidad
 	where Rol_ID=@Rol_ID	
 end
