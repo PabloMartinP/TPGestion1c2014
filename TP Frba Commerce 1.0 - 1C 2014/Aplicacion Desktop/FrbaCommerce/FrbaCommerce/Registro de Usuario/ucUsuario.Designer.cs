f458c32591e8e0453b82d91be8949cd01c4fd834
PabloMartinP <pablo.penialoza@gmail.com>
2014-05-08 13:41:53 -0300

    Agregad UseControl ucUsuario

---
+++ TP Frba Commerce 1.0 - 1C 2014/Aplicacion Desktop/FrbaCommerce/FrbaCommerce/Registro de Usuario/ucUsuario.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace FrbaCommerce.RegistrarUsuario
+{
+    partial class ucUsuario
+    {
+        /// <summary> 
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar 
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtRePassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtRol = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(3, 4);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(57, 13);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "UserName";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(85, 1);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(141, 20);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(85, 26);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(141, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // txtRePassword
+            // 
+            this.txtRePassword.Location = new System.Drawing.Point(85, 52);
+            this.txtRePassword.Name = "txtRePassword";
+            this.txtRePassword.Size = new System.Drawing.Size(141, 20);
+            this.txtRePassword.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Re-Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Rol Asignado";
+            // 
+            // txtRol
+            // 
+            this.txtRol.Location = new System.Drawing.Point(85, 78);
+            this.txtRol.Name = "txtRol";
+            this.txtRol.Size = new System.Drawing.Size(141, 20);
+            this.txtRol.TabIndex = 8;
+            // 
+            // ucUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtRol);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtRePassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblUserName);
+            this.Name = "ucUsuario";
+            this.Size = new System.Drawing.Size(229, 101);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtRePassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtRol;
+    }
+}
---
+++ TP Frba Commerce 1.0 - 1C 2014/Aplicacion Desktop/FrbaCommerce/FrbaCommerce/Registro de Usuario/ucUsuario.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FrbaCommerce.Controller;
+using FrbaCommerce.Entity;
+
+namespace FrbaCommerce.RegistrarUsuario
+{
+    public partial class ucUsuario : UserControl
+    {
+        private Usuario.eTipo _tipo;
+        public Usuario.eTipo Tipo
+        {
+            get { return _tipo; }
+            set
+            {
+                switch (value)
+                {
+                    case Usuario.eTipo.Cliente:
+                        this.txtRol.Text = "Cliente";
+                        break;
+                    case Usuario.eTipo.Empresa:
+                        this.txtRol.Text = "Empresa";
+                        break;
+                    default:
+                        break;
+                }
+                _tipo = value;
+            }
+        }
+
+        public Usuario Usuario 
+        {
+            get 
+            {
+                Usuario unUser = new Usuario();
+
+                unUser.UserName = txtUserName.Text;
+                unUser.Password = txtPassword.Text;
+                unUser.Tipo = this.Tipo;
+
+                return unUser;
+            }
+        }
+
+        public ucUsuario()
+        {
+            InitializeComponent();
+        }
+        public bool Validar(out string errores)        
+        {            
+            errores = "";
+            
+            //UsuarioController uc = new UsuarioController();
+            //Usuario u = new Usuario();
+            //u.UserName = txtUserName.Text;
+
+            //uc.Validar(u,out errores);
+
+            if (txtPassword.Text != txtRePassword.Text)
+                errores = errores + "\nLos Passwords no coincide.";
+
+            return errores == "";
+        }
+                      
+       
+    }
+}
